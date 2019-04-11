ae5c44f70c27650bd1c35095320c743e62ebbed3
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-04-11 20:16:38 +1200

    Use secure url for "opensource.org".

--- playground/widgets.py
+++ playground/widgets.py
@@ -17,7 +17,7 @@ from common import NUKE_COLORMATRIX_NODE_TEMPLATE, nuke_format_matrix
 
 __author__ = 'Colour Developers'
 __copyright__ = 'Copyright (C) 2018-2019 - Colour Developers'
-__license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
+__license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
 __status__ = 'Production'
